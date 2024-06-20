2e567ef8b967f72bf754b7696709c8aaed0045d1
Sean Derrow <sean.derrow@gmail.com>
2024-06-20 19:23:33 -0400

    Revert "Reset to original"
    This reverts commit e4ac3f4a84c065b8a3843d4fc077b812cce105ee.

--- verify_tla.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -Eeuox pipefail
-mkdir -p build_tla
-cd build_tla
-sed -n 's@.*//^ \(.*\)@\1@p' ../native.cc > EventLoopBlockage.tla
-pcal EventLoopBlockage.tla
-tlc EventLoopBlockage.tla
